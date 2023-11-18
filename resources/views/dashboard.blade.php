3984f3e52b743f61636ac26850426f55e567b143
Miguel Usma <usmamiguel04@gmail.com>
2023-11-18 16:04:06 -0500

    Carrera Controller

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,9 +9,15 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+                    {{-- {{ __("You're logged in!") }} --}}
+
+                    <h1 class="text-center text-4xl font-bold">Bienvenido a la BD de la FCECEP!</h1>
+<br>
+                    <p class="text-center">En está aplicación web podrás consultar los estudiantes que se encuentran registrados en las distintas carreras.</p>
                 </div>
             </div>
+
+            
         </div>
     </div>
 </x-app-layout>
