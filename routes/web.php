73f251e1e27b52c053bcc52a279b301b3357db89
Miguel Usma <usmamiguel04@gmail.com>
2023-11-19 18:18:06 -0500

    Rutas Protegidas usuarios Autenticados

--- routes/web.php
+++ routes/web.php
@@ -30,7 +30,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    // Route::get('/carreras', [CarreraController::class, 'index'])->name('carrera.index');
+    // Route::get('/carreras', [CarreraController::class, 'index'])->name('carrera.index'
     Route::resource('carrera',CarreraController::class);
     Route::resource('estudiante',EstudianteController::class);
 });
