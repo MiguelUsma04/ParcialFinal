b811cca43d64617b9476e3e19f080c21076fdb72
Miguel Usma <usmamiguel04@gmail.com>
2023-11-19 19:55:04 -0500

    Instalacion y Configuracion Vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,13 @@
 import './bootstrap';
-
 import Alpine from 'alpinejs';
+import Vue from 'vue/dist/vue.esm.js';
+import CarrerasComponent from './components/CarrerasComponent.vue';
 
 window.Alpine = Alpine;
-
 Alpine.start();
+
+Vue.component('carreras-component', CarrerasComponent);
+
+const app = new Vue({
+    el: '#app',
+});
